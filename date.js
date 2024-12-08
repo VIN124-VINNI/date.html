df9fccc38bf691da68622790d1667bd6f010c82e
VIN124-VINNI <vineelareddy925@gmail.com>
2024-12-08 10:48:32 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,36 @@
+// Date methods:
+let a=new Date();
+console.log(a.getFullYear());
+console.log(a.getHours())
+console.log(a.getMinutes())
+console.log(a.getMilliseconds())
+console.log(a.toDateString())
+console.log(a.toTimeString())
+console.log(a.toLocaleDateString())
+console.log(a.toLocaleTimeString())
+let b=new Date();
+b.setDate(20)
+b.setFullYear(2025)
+console.log(b.getFullYear())
+console.log(b.toDateString())
+console.log(b.toTimeString())
+console.log(b.toLocaleDateString())
+console.log(b.toLocaleTimeString())
+// math methods:
+var a1=4;
+console.log(Math.round(a))
+let s=3.5;
+console.log(Math.ceil(s))
+let s1=7.4;
+console.log(Math.floor(s1))
+let s2=144;
+console.log(Math.sqrt(s2))
+let s3=12;
+console.log(Math.pow(s3,7))
+console.log(Math.random()*10)
+console.log(Math.max(2,3,-4,9))
+console.log(Math.min(3,8,67))
+console.log(Math.min(2,5,7))
+console.log(Math.abs(-5))
+
+
